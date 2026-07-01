6172d6acbc503c64cd9e90cbaa85e8889a5281b1
fgwsz <3191000458@qq.com>
2026-07-01 18:42:12 +0800

    增加build.sh可执行权限,使用python3调用

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 dir_path=$(dirname "$(readlink -f "$0")")
 
 pip install pyinstaller
-python -m PyInstaller --onefile "$dir_path/fgwsz-package.py" \
+python3 -m PyInstaller --onefile "$dir_path/fgwsz-package.py" \
     --distpath "$dir_path/build/linux/dist" \
     --workpath "$dir_path/build/linux/work" \
     --specpath "$dir_path/build/linux/spec" \
